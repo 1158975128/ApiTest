4812fc4d8f55181366c896445609d85062af9f3b
金灵 <ling.jin@fftai.com>
2021-05-27 14:51:59 +0800

    update test_JMeter.bat

--- scripts/test_JMeter.bat
+++ scripts/test_JMeter.bat
@@ -4,3 +4,5 @@ set jmx=HTTP Request.jmx
 set jtl=jmeter_%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%.jtl
 
 jmeter -n -t "D:\DevOps\Test_JMeter\%jmx%" -l "D:\DevOps\Test_JMeter\%jtl%"
+
+pause
