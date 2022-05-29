e2c6d2f406d8a29bc28988692a335b103616f8de
LX0 <xiu.li@fftai.com>
2022-05-29 14:59:49 +0800

    测试医生角色的患者管理功能

--- page_objects/patient/patient_list.py
+++ page_objects/patient/patient_list.py
@@ -7,34 +7,114 @@ from config.public_data.delay_time import *
 from utils.object_map import ObjectMap
 from common.logger import MyLogging
 from page_objects.navigate_bar import NavigateBar
-from page_objects.navigate_bar_all.navigate_version import VersionExpanded
+import pytest_check as check
 
 log = MyLogging(__name__).logger
 map_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../page_element"))
-patient_map = map_path + "/patient/patient_manage.xml"
+patient_list_map = map_path + "/patient/patient_list.xml"
 
 delay_time = DelayTime.short_time.value
 
 
-class Patient_Manage():
+class Patient_List():
     def __init__(self, driver):
         self.driver = driver
-        self.version = ObjectMap(patient_map)
+        self.version = ObjectMap(patient_list_map)
         self.patient = NavigateBar(self.driver)
 
-    def check_patient_page(self):
+    def check_doctor_patient_page(self,data):
         '''
         检查页面元素是否正确
+        '''
+        self.patient.go_to_patient()
+        time.sleep(1)
+        page_list = []
+        page_list.append(self.version.getLocator(self.driver, "New_patient").text)
+        page_list.append(self.version.getLocator(self.driver, "Origin_droplist").get_attribute('value'))
+        page_list.append(self.version.getLocator(self.driver, "Patient_droplist").get_attribute('value'))
+        page_list.append(self.version.getLocator(self.driver, "Name_input").get_attribute('placeholder'))
+        page_list.append(self.version.getLocator(self.driver, "Date").get_attribute('placeholder'))
+        page_list.append(self.version.getLocator(self.driver, "Date_Frame").text)
+        page_list.append(self.version.getLocator(self.driver, "Check_End_Date").get_attribute('placeholder'))
+        for i in range(len(page_list)):
+            check.equal(page_list[i],data[i],"检查页面按钮名称和默认设置是否正确")
+        log.info("页面按钮名称和默认设置正确")
+
+    def check_patient_card(self):
+        '''
+        检查患者卡片页面元素是否正确
         :return: new_patient,origin_droplist,patient_droplist
         '''
         self.patient.go_to_patient()
         time.sleep(1)
-        new_patient = self.version.getLocator(self.driver, "New_patient").text
-        origin_droplist = self.version.getLocator(self.driver, "Origin_droplist").get_attribute('value')
-        patient_droplist = self.version.getLocator(self.driver, "Patient_droplist").get_attribute('value')
-        return new_patient,origin_droplist,patient_droplist
+        source = self.version.getLocator(self.driver, "Source").get_attribute('textContent').strip()
+        sex_logo = self.version.getLocator(self.driver, "Sex_logo").get_attribute('class').strip()
+        check_name = self.version.getLocator(self.driver, "Check_name").get_attribute('textContent').strip()
+        patient_department = self.version.getLocator(self.driver, "Patient_Department").get_attribute('textContent').strip()
+        sex_value = self.version.getLocator(self.driver, "Sex_Value").get_attribute('textContent').strip()
+        attending_doctor = self.version.getLocator(self.driver, "Attending_Doctor").get_attribute('textContent').strip()
+        cost = self.version.getLocator(self.driver, "Cost").get_attribute('textContent').strip()
+        registration_time = self.version.getLocator(self.driver, "Registration_Time").get_attribute('textContent').strip()
+        disease_name = self.version.getLocator(self.driver, "Disease_Name").get_attribute('textContent').strip()
+        therapeutist = self.version.getLocator(self.driver, "Therapeutist").get_attribute('textContent').strip()
+        appoint_therapeutist = self.version.getLocator(self.driver, "Appoint_Therapeutist").get_attribute('textContent').strip()
+        see_report = self.version.getLocator(self.driver, "See_Report").get_attribute('textContent').strip()
 
-    def add_new_patient(self,patient,disease,diagnosis):
+
+        print(source,sex_logo,check_name,patient_department,sex_value,attending_doctor,cost,registration_time,disease_name,therapeutist,appoint_therapeutist,see_report)
+
+    def appoint_therapeutist(self):
+        self.patient.go_to_patient()
+        self.driver.implicitly_wait(20)
+        appoint = self.version.getLocator(self.driver, "Appoint_Therapeutist")
+        appoint.click()
+        choose = self.version.getLocator(self.driver, "Choose_Therapeutist")
+        choose.click()
+        choose.send_keys("周治疗师全")
+        select = self.version.getLocator(self.driver, "Select_Therapeutist")
+        select.click()
+        ensure = self.version.getLocator(self.driver, "Ensure")
+        ensure.click()
+        again_ensure = self.version.getLocator(self.driver, "Again_Ensure")
+        again_ensure.click()
+        time.sleep(1)
+        tips = self.version.getLocator(self.driver, "Tips").text.strip()
+        appoint_therapeutist = self.version.getLocator(self.driver, "Appoint_Therapeutist").get_attribute('textContent').strip()
+        check.equal(tips,"指定治疗师成功","检查治疗师指定成功")
+        check.equal(appoint_therapeutist,"修改治疗师","检查按钮是否变为修改治疗师")
+
+    def verify_paging(self):
+        '''
+        测试分页功能，如果当前页面卡片>12张时进行翻页操作
+        '''
+        self.patient.go_to_patient()
+        time.sleep(3)
+        select_all = self.version.getLocator(self.driver, "Select_All")
+        select_all.click()
+        cards = self.driver.find_elements(By.CSS_SELECTOR,value=".el-row>div")
+        next_page = self.version.getLocator(self.driver, "Next_Page")
+        if len(cards) == 12 and next_page.is_displayed():
+            next_page.click()
+            log.info("翻页成功，已进入下一页")
+        else:
+            log.info("当前页面卡片小于或等于12张，无法进行翻页操作")
+
+
+    def verify_screen(self,patient_type):
+        sources = self.driver.find_elements(By.CSS_SELECTOR,value=".el-row>div .card-source")
+        print("当前页面有",len(sources),patient_type)
+        if len(sources) >1:
+            for source in sources:
+                print(source.get_attribute('textContent').strip(),"len!=1")
+                check.equal(source.get_attribute('textContent').strip(),patient_type,"检查传入的患者类型与页面显示的患者类型是否一致")
+        elif len(sources) == 1:
+            check.equal(sources.get_attribute('textContent').strip(), patient_type,"检查传入的患者类型与页面显示的患者类型是否一致")
+            print(sources.get_attribute('textContent').strip(),"len==1")
+        elif len(sources) == 0:
+            log.info("当前%s没有患者"%patient_type)
+
+
+    def add_new_patient(self,patient,disease,diagnosis,patient_sex,patient_origin):
         '''
         新增一个住院患者
         :param patient: 传入患者姓名姓名
@@ -48,8 +128,12 @@ class Patient_Manage():
         new_patient.click()
         patient_name = self.version.getLocator(self.driver, "Patient_name")
         patient_name.send_keys(patient)
-        sex = self.version.getLocator(self.driver, "Sex_man")
-        sex.click()
+        if patient_sex == "男":
+            sex_man = self.version.getLocator(self.driver, "Sex_man")
+            sex_man.click()
+        elif patient_sex == "女":
+            sex_woman = self.version.getLocator(self.driver, "Sex_Woman")
+            sex_woman.click()
         disease_type = self.version.getLocator(self.driver, "Disease_type")
         disease_type.click()
         time.sleep(1)
@@ -57,9 +141,14 @@ class Patient_Manage():
         disease_type_select = self.version.getLocator(self.driver, "Disease_type_select")
         disease_type_select.click()
         time.sleep(2)
-        origin = self.version.getLocator(self.driver, "Origin")
-        origin.click()
-        time.sleep(1)
+        if patient_origin == "住院":
+            origin_hospital = self.version.getLocator(self.driver, "Origin_hospital")
+            origin_hospital.click()
+            time.sleep(1)
+        elif patient_origin == "其他":
+            origin_other = self.version.getLocator(self.driver, "Origin_Other")
+            origin_other.click()
+            time.sleep(1)
         function_diagnosis = self.version.getLocator(self.driver, "Function_diagnosis")
         function_diagnosis.click()
         time.sleep(1)
@@ -84,28 +173,105 @@ class Patient_Manage():
         time.sleep(1)
         tips = self.version.getLocator(self.driver, "Tips").text.strip()
         if tips == '登记患者成功！':
-            print("新增患者成功：", tips)
             return True
         else:
             return False
         log.info("新增成功")
 
-    def find_patient(self,patient_name):
+    def find_patient(self,patient_name=None,patient_origin='all',patient_type="全部患者"):
         '''
-        查找住院患者
+        查找患者
         :param patient_name: 传入查找的患者姓名
         '''
         self.patient.go_to_patient()
         time.sleep(3)
-        origin_select = self.version.getLocator(self.driver, "Origin_select")
-        origin_select.click()
+        select_all = self.version.getLocator(self.driver, "Select_All")
+        select_all.click()
         time.sleep(1)
-        hospital_select = self.version.getLocator(self.driver, "Hospital_select")
-        hospital_select.click()
+        if patient_origin == "门诊":
+            origin_select = self.version.getLocator(self.driver, "Origin_select")
+            origin_select.click()
+            time.sleep(1)
+            outpatient_select = self.version.getLocator(self.driver, "Outpatient_select")
+            outpatient_select.click()
+        elif patient_origin == "住院":
+            origin_select = self.version.getLocator(self.driver, "Origin_select")
+            origin_select.click()
+            hospital_select = self.version.getLocator(self.driver, "Hospital_select")
+            hospital_select.click()
+        elif patient_origin == "其他":
+            origin_select = self.version.getLocator(self.driver, "Origin_select")
+            origin_select.click()
+            other_select = self.version.getLocator(self.driver, "Other_select")
+            other_select.click()
+
+        if patient_type == "全部患者":
+            pass
+        elif patient_type == "我的患者":
+            patient_all = self.version.getLocator(self.driver, "Patient_All")
+            patient_all.click()
+            my_patient = self.version.getLocator(self.driver, "My_Patient")
+            my_patient.click()
         time.sleep(1)
-        name_input = self.version.getLocator(self.driver, "Name_input")
-        name_input.send_keys(patient_name)
-        time.sleep(1)
+
+        if patient_name == None:
+            pass
+        else:
+            name_input = self.version.getLocator(self.driver, "Name_input")
+            name_input.clear()
+            name_input.send_keys(patient_name)
+            time.sleep(1)
+
+    def check_patient_name(self,patient_name):
+        check_names =self.driver.find_elements(By.CSS_SELECTOR,value=".el-row>div .card-head .title")
+        if len(check_names) >=1:
+            for check_name in check_names:
+                check_name = check_name.text
+                if patient_name == check_name:
+                    check.equal(patient_name,check_name,"检查患者查找成功")
+                    log.info("已找到名为%s的患者"%patient_name)
+                    return True
+                elif patient_name != check_name:
+                    log.info("输入查找的患者为%s实际找到的患者为%s"%(patient_name,check_name))
+                    return False
+        else:
+            log.info("没有叫%s的患者"%patient_name)
+            return False
+
+    def choose_date(self,choose_month="current"):
+        date_logo = self.version.getLocator(self.driver, "Date")
+        date_logo.click()
+        month = self.version.getLocator(self.driver, "Month").get_attribute('textContent').split("年")[1].strip()
+        split_month = month.split("月")[0].strip()
+        if choose_month != "current":
+            last_month = self.version.getLocator(self.driver, "Last_Month")
+            last_month.click()
+            self.driver.implicitly_wait(30)
+            beginday = self.version.getLocator(self.driver, "Oneday")
+            beginday.click()
+            lastday = self.version.getLocator(self.driver, "Lastmonth_day")
+            lastday.click()
+            time.sleep(1)
+        else:
+            beginday = self.version.getLocator(self.driver, "Oneday")
+            beginday.click()
+            lastday = self.version.getLocator(self.driver, "Lastday")
+            lastday.click()
+            time.sleep(1)
+        check_dates = self.driver.find_elements(By.CSS_SELECTOR,value='.el-row>div [class="patient-info patient-register"]>p:nth-child(3)>span')
+        if len(check_dates) >= 1:
+            for i in range(len(check_dates)):
+                registration_time = self.version.getLocator(self.driver, "Registration_Time").get_attribute('textContent').split("年0")[1]
+                split_registration_time = registration_time.split("月")[0]
+                check.equal(split_month, split_registration_time, "检查当前页面患者的月份于所查找的是否一直")
+            log.info('匹配成功')
+
+        else:
+            log.info('当前日期无患者')
+
+
+
+
 
 
     def add_treatment_item(self,patient_name,item):
@@ -116,7 +282,6 @@ class Patient_Manage():
         :return: 成功返回True ，失败返回False
         '''
         check_name = self.version.getLocator(self.driver, "Check_name")
-        print(check_name.text,patient_name)
         if patient_name == check_name.text:
             check_name.click()
             time.sleep(1)
@@ -147,7 +312,6 @@ class Patient_Manage():
             time.sleep(1)
             tips = self.version.getLocator(self.driver, "Tips").text
             if tips == "新增项目成功":
-                print("新增治疗项目成功：",tips)
                 return True
             else:
                 return False
@@ -227,7 +391,6 @@ class Patient_Manage():
             time.sleep(1)
             tips = self.version.getLocator(self.driver, "Tips").text
             if tips == "结束疗程成功":
-                print("结束疗程成功：",tips)
                 return True
             else:
                 return False
--- test_case/fris_patient_list_doctor.py
+++ test_case/fris_patient_list_doctor.py
@@ -20,9 +20,20 @@ class Test_Patient_List():
     def teardown(self):
         self.Patient = Patient_List(self.driver)
         self.driver.quit()
+
     # 需求号S#1687
+    @pytest.mark.parametrize('data',[('新增患者', '门诊', '全部患者', '姓名', '开始日期', '至', '结束日期')])
+    def test_01_check_doctor_patient_page(self,data):
+        '''
+        测试页面按钮名称和默认设置是否正确
+        :param data: 按钮名称和默认设置
+        '''
+        self.patient = Patient_List(self.driver)
+        self.patient.check_doctor_patient_page(data)
+
+
     @pytest.mark.parametrize('data',[("测试其他女","关节炎","关","女","其他"),("测试住院男","关节炎","关","男","住院")])
-    def test_01_check_patient_card(self, data):
+    def test_02_check_patient_card(self, data):
         '''
         测试患者卡片显示是否正确
         :param data: 传入新增患者信息
@@ -35,7 +46,7 @@ class Test_Patient_List():
         self.patient.leave_hospital(data[0])
 
     @pytest.mark.parametrize('data',[("测试治疗师","关节炎","关","女","其他","ui_test"),("测试治疗师","关节炎","关","男","住院","ui_test")])
-    def test_02_appoint_therapeutist(self, data):
+    def test_03_appoint_therapeutist(self, data):
         '''
         测试指定治疗师
         :param data:传入新增患者信息
@@ -51,7 +62,7 @@ class Test_Patient_List():
         time.sleep(1)
         self.patient.leave_hospital(patient)
 
-    def test_03_verify_paging(self):
+    def test_04_verify_paging(self):
         '''
         测试翻页功能
         '''
@@ -59,7 +70,7 @@ class Test_Patient_List():
         self.patient.verify_paging()
 
     @pytest.mark.parametrize('data',[("门诊","我的患者","current"),("住院","我的患者","last"),("其他","我的患者","current"),("门诊","全部患者","last"),("住院","全部患者","last"),("其他","全部患者","current")])
-    def test_04_verify_screen(self,data):
+    def test_05_verify_screen(self,data):
         '''
         测试登记时间、患者来源和患者类型筛选功能
         '''
@@ -74,7 +85,7 @@ class Test_Patient_List():
 
 
     @pytest.mark.parametrize('data',[("其他女","关节炎","关","女","其他","我的患者"),("住院男","关节炎","关","男","住院","全部患者")])
-    def test_04_verify_name_screen(self,data):
+    def test_06_verify_name_screen(self,data):
         '''
         测试患者姓名、患者来源和患者类型组合筛选功能
         '''
@@ -92,13 +103,3 @@ class Test_Patient_List():
             self.driver.implicitly_wait(100)
             self.patient.leave_hospital(patient)
             self.driver.implicitly_wait(30)
-
-    # @pytest.mark.parametrize('data',[("其他女","关节炎","关","女","其他","我的患者"),("住院男","关节炎","关","男","住院","全部患者")])
-    def test_05_verify_date(self):
-        '''
-        测试患者姓名、患者来源和患者类型组合筛选功能
-        '''
-        self.patient = Patient_List(self.driver)
-        self.patient.find_patient()
-        time.sleep(1)
-        self.patient.choose_date()
