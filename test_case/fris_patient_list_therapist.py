23d761c89cc1d2f395e11f54da3ea502e87c799e
LX0 <xiu.li@fftai.com>
2022-05-31 12:29:39 +0800

    治疗师--患者管理

--- test_case/fris_patient_list_therapist.py
+++ test_case/fris_patient_list_therapist.py
@@ -15,41 +15,22 @@ class Test_Patient_List():
     def setup(self):
         self.driver = Browser.open_browser()
         self.mylogin = LoginPage(self.driver)
-        self.mylogin.login_fris('doctor')
+        self.mylogin.login_fris('therapistall')
 
     def teardown(self):
         self.Patient = Patient_List(self.driver)
         self.driver.quit()
-    # 需求号S#1687
-    @pytest.mark.parametrize('data',[("测试其他女","关节炎","关","女","其他"),("测试住院男","关节炎","关","男","住院")])
-    def test_01_check_patient_card(self, data):
-        '''
-        测试患者卡片显示是否正确
-        :param data: 传入新增患者信息
-        '''
-        self.patient = Patient_List(self.driver)
-        print(data)
-        self.patient.add_new_patient(data[0],data[1],data[2],data[3],data[4])
-        self.patient.find_patient(data[0],data[4])
-        self.patient.check_patient_card()
-        self.patient.leave_hospital(data[0])
 
-    @pytest.mark.parametrize('data',[("测试治疗师","关节炎","关","女","其他","ui_test"),("测试治疗师","关节炎","关","男","住院","ui_test")])
-    def test_02_appoint_therapeutist(self, data):
+    # 需求号S#1687
+    @pytest.mark.parametrize('data',[('门诊', '我的患者', '姓名', '开始日期', '至', '结束日期')])
+    def test_01_check_doctor_patient_page(self,data):
         '''
-        测试指定治疗师
-        :param data:传入新增患者信息
+        测试页面按钮名称和默认设置是否正确
+        :param data: 按钮名称和默认设置
         '''
         self.patient = Patient_List(self.driver)
-        now_time = time.strftime("%Y_%m_%d_%H_%M_%S")
-        patient = data[0] + now_time
-        self.patient.add_new_patient(patient,data[1],data[2],data[3],data[4])
-        self.patient.find_patient(patient)
-        self.patient.add_treatment_item(patient,data[5])
-        self.patient.find_patient(patient)
-        self.patient.appoint_therapeutist()
-        time.sleep(1)
-        self.patient.leave_hospital(patient)
+        self.patient.check_therapist_patient_page(data)
+
 
     def test_03_verify_paging(self):
         '''
@@ -58,7 +39,7 @@ class Test_Patient_List():
         self.patient = Patient_List(self.driver)
         self.patient.verify_paging()
 
-    @pytest.mark.parametrize('data',[("门诊","我的患者","current"),("住院","我的患者","last"),("其他","我的患者","current"),("门诊","全部患者","last"),("住院","全部患者","last"),("其他","全部患者","current")])
+    @pytest.mark.parametrize('data',[("门诊","全部患者","last"),("门诊","我的患者","current"),("住院","我的患者","last"),("其他","我的患者","current"),("住院","全部患者","last"),("其他","全部患者","current")])
     def test_04_verify_screen(self,data):
         '''
         测试登记时间、患者来源和患者类型筛选功能
@@ -73,32 +54,15 @@ class Test_Patient_List():
 
 
 
-    @pytest.mark.parametrize('data',[("其他女","关节炎","关","女","其他","我的患者"),("住院男","关节炎","关","男","住院","全部患者")])
+    @pytest.mark.parametrize('data',[('测试治疗师2022_05_30_14_04_42',"其他","我的患者"),("检查住院男","住院","全部患者")])
     def test_04_verify_name_screen(self,data):
         '''
         测试患者姓名、患者来源和患者类型组合筛选功能
         '''
         self.patient = Patient_List(self.driver)
-        now_time = time.strftime("%Y_%m_%d_%H_%M_%S")
-        patient = data[0] + now_time
-        self.patient.add_new_patient(patient, data[1], data[2], data[3], data[4])
-        self.patient.find_patient(patient, data[4], data[5])
-        time.sleep(1)
-        self.patient.choose_date()
-        time.sleep(1)
-        result = self.patient.check_patient_name(patient)
-        self.driver.implicitly_wait(30)
-        if result:
-            self.driver.implicitly_wait(100)
-            self.patient.leave_hospital(patient)
-            self.driver.implicitly_wait(30)
 
-    # @pytest.mark.parametrize('data',[("其他女","关节炎","关","女","其他","我的患者"),("住院男","关节炎","关","男","住院","全部患者")])
-    def test_05_verify_date(self):
-        '''
-        测试患者姓名、患者来源和患者类型组合筛选功能
-        '''
-        self.patient = Patient_List(self.driver)
-        self.patient.find_patient()
+        self.patient.find_patient(data[0], data[1], data[2])
         time.sleep(1)
         self.patient.choose_date()
+        time.sleep(1)
+        self.patient.check_patient_name(data[0])
