c98449d91a4908e254ab9ce967c7041ed9046c8b
秀鹅 <xiu.li@fftai.com>
2022-06-16 11:53:34 +0800

    S#1969治疗维护-疾病类型

--- page_objects/treatment_preserve/disease_type.py
+++ page_objects/treatment_preserve/disease_type.py
@@ -1,17 +1,17 @@
 import os
 import time
-
+import random
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from config.public_data.delay_time import *
 from utils.object_map import ObjectMap
 from common.logger import MyLogging
+from page_objects.navigate_bar import NavigateBar
 from page_objects.navigate_bar_all.navigate_version import VersionExpanded
 
 log = MyLogging(__name__).logger
 map_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../page_element"))
-disease_type_map = map_path + "/treatment_preserve/disease_type.xml"
-
+disease_map = map_path + "/treatment_preserve/disease_type.xml"
 
 delay_time = DelayTime.short_time.value
 
@@ -19,39 +19,145 @@ delay_time = DelayTime.short_time.value
 class Disease_Type():
     def __init__(self, driver):
         self.driver = driver
-        self.version = ObjectMap(disease_type_map)
-        # self.management = VersionExpanded(self.driver)
-        # self.management.navigate_version_control()
+        self.version = ObjectMap(disease_map)
+        self.disease_page = NavigateBar(self.driver)
 
-    def add_new_disease(self):
-        treatment_preserve = self.version.getLocator(self.driver, "Treatment_Preserve")
-        treatment_preserve.click()
-        disease_type = self.version.getLocator(self.driver, "Disease_Type")
-        disease_type.click()
+
+    def check_disease_page(self):
+        '''
+        检查页面元素是否正确
+        :return: new_additional,check_department_name,check_operation_name
+        '''
+        self.disease_page.go_to_disease()
+        time.sleep(1)
+        new_additional = self.version.getLocator(self.driver, "New_Additional").text.strip()
+        Check_Position_Names = self.driver.find_elements(By.CSS_SELECTOR,value='.el-table__header-wrapper th div')
+        position = Check_Position_Names[0].get_attribute('textContent').strip()
+        create_time = Check_Position_Names[1].get_attribute('textContent').strip()
+        modification_time = Check_Position_Names[2].get_attribute('textContent').strip()
+        operation = Check_Position_Names[3].get_attribute('textContent').strip()
+        return new_additional,position,create_time,modification_time,operation
+
+
+    def add_new_disease(self,disease_name):
+        '''
+        新增疾病类型
+        :param disease_name: 新增疾病的名称
+        '''
+        self.disease_page.go_to_disease()
+        self.driver.implicitly_wait(20)
         new_additional = self.version.getLocator(self.driver, "New_Additional")
         new_additional.click()
         import_words = self.version.getLocator(self.driver, "Import_Words")
-        import_words.send_keys("测试新增疾病类型")
+        import_words.send_keys(disease_name)
         ensure = self.version.getLocator(self.driver, "Ensure")
         ensure.click()
-        new_disease_name = self.version.getLocator(self.driver, 'Disease_name').text
-        return new_disease_name
+        time.sleep(1)
         log.info("新增成功")
 
-    def add_none_disease(self):
-        treatment_preserve = self.version.getLocator(self.driver, "Treatment_Preserve")
-        treatment_preserve.click()
-        disease_type = self.version.getLocator(self.driver, "Disease_Type")
-        disease_type.click()
-        new_additional = self.version.getLocator(self.driver, "New_Additional")
-        new_additional.click()
-        import_words = self.version.getLocator(self.driver, "Import_Words")
-        import_words.send_keys("")
-        ensure = self.version.getLocator(self.driver, "Ensure")
-        ensure.click()
-        new_disease_name = self.version.getLocator(self.driver, 'Disease_name').text
-        return new_disease_name
-
-    def delete_disease(self):
+    def find_disease_name(self,name):
+        """
+        查找部门名称是否在列表中
+        :param name: 部门名称
+        :return: True/Flase
+        """
+        self.disease_page.go_to_disease()
+        disease_names = self.driver.find_elements(By.CSS_SELECTOR,value='.is-scrolling-none tr td:nth-child(1) div')
         next_page = self.version.getLocator(self.driver, "Next_Page")
+        name_list = []
+        flag = True
+        status = 0
+        while flag:
+            # print('执行了if')
+            if len(disease_names) == 10 and next_page.is_displayed():
+                for disease_name in disease_names:
+                    name_list.append(disease_name.text.strip())
+                if name in name_list:
+                    flag = False
+                    return True
+                else:
+                    next_page.click()
+                # print(name_list,status,flag)
+            else:
+                # print('执行了else')
+                for disease_name in disease_names:
+                    # print(disease_name.get_attribute('textContent').strip(),flag)
+                    name_list.append(disease_name.get_attribute('textContent').strip())
+                for disease_name in name_list:
+                    if disease_name == name:
+                        status = 1
+                # print(name_list)
+                if status == 1:
+                    return True
+                else:
+                    print('没有匹配的' + name)
+                    return False
+                flag = False
+                print(flag,'*******')
 
+
+    def delete_disease(self,disease_name):
+        '''
+        删除部门
+        :param department_name: 传入删除的部门名称
+        Logic：遍历Tbody,根据传入的删除的部门名称找到对应的删除按钮，删除后跳出循环
+        :except
+        '''
+        try:
+            self.disease_page.go_to_disease()
+            time.sleep(1)
+            table = self.version.getLocator(self.driver, 'Table')
+            table_trs = table.find_elements(By.TAG_NAME, value='tr')
+            for i in range(len(table_trs)):
+                table_tr_td1s = table_trs[i].find_elements(By.TAG_NAME, value='td')
+                for table_tr_td1 in table_tr_td1s:
+                    #     # 根据部门名称定位要删除的行
+                    if table_tr_td1.get_attribute('textContent').strip() == disease_name:
+                        table_tr_td2s = table_trs[i].find_elements(By.TAG_NAME, value='td')
+                        #         # 获取tr里第二个td的所有button
+                        table_buttons = table_tr_td2s[3].find_elements(By.TAG_NAME, value='button')
+                        for table_button in table_buttons:
+                            if table_button.text == "删除":
+                                table_button.click()
+                                time.sleep(1)
+                                self.driver.implicitly_wait(10)
+                                ensure = self.version.getLocator(self.driver, "Delete_Ensure")
+                                ensure.click()
+                                time.sleep(1)
+                        # 防止删除成功后少行超出范围
+                        break
+            log.info("删除成功")
+        except:
+            pass
+
+    def modify_disease(self,disease_name,change_disease):
+        '''
+        修改部门名称
+        :param department_name: 传入删除的部门名称
+        Logic：遍历Tbody,根据传入的修改的部门名称找到对应的删除按钮，修改后跳出循环
+        '''
+        self.disease_page.go_to_disease()
+        time.sleep(3)
+        table = self.version.getLocator(self.driver, 'Table')
+        table_trs = table.find_elements(By.TAG_NAME,value='tr')
+        for i in range(len(table_trs)):
+            table_tr_td1s = table_trs[i].find_elements(By.TAG_NAME,value='td')
+            for table_tr_td1 in table_tr_td1s:
+            #     # 根据部门名称定位要删除的行
+                if table_tr_td1.get_attribute('textContent').strip() == disease_name:
+                    table_tr_td2s = table_trs[i].find_elements(By.TAG_NAME,value='td')
+            #         # 获取tr里第二个td的所有button
+                    table_buttons = table_tr_td2s[3].find_elements(By.TAG_NAME,value='button')
+                    for table_button in table_buttons:
+                        if table_button.text == "修改":
+                            table_button.click()
+                            self.driver.implicitly_wait(20)
+                            import_words = self.version.getLocator(self.driver, "Import_Words")
+                            import_words.clear()
+                            import_words.send_keys(change_disease)
+                            ensure = self.version.getLocator(self.driver, "Ensure")
+                            ensure.click()
+                            time.sleep(1)
+                    # 修改成功后跳出循环
+                    break
+        log.info("修改成功")
--- test_case/fris_disease_type.py
+++ test_case/fris_disease_type.py
@@ -1,42 +1,115 @@
-import os
+import time
 import unittest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import pytest
 from page_objects.login.login_page import LoginPage
 from utils.browser_tool import Browser
 from common.logger import MyLogging
 from parameterized import parameterized
 from page_objects.treatment_preserve.disease_type import Disease_Type
+import pytest_check as check
+
 
 log = MyLogging(__name__).logger
 
-def my_sum(a,b):
-    return  a+b
-class TestDiseaseType(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.driver = Browser.open_browser()
-        cls.mylogin = LoginPage(cls.driver)
-        cls.mylogin.login_fris()
 
-    def test_01_disease_type_page(self):
-        print("TestCase:验证疾病类型页格式正确<br/>")
+class TestDisease():
 
+    def setup(self):
+        self.driver = Browser.open_browser()
+        self.mylogin = LoginPage(self.driver)
+        self.mylogin.login_fris('admin')
 
+    def teardown(self):
+        self.Type = Disease_Type(self.driver)
+        self.driver.quit()
 
-    @parameterized.expand([("","测试新增疾病类型", True), ("", "", False)])
-    def test_02_version_update(self, test_case, disease_name, result):
-        print("TestCase:" + test_case)
+    data = [('新增疾病类型','疾病类型名称','创建时间','修改时间','操作')]
+    @pytest.mark.parametrize('new_additional,position,create_time,modification_time,operation',data)
+    def test_01_disease_page(self,new_additional,position,create_time,modification_time,operation):
+        '''
+        验证职务页格式正确
+        '''
         self.type = Disease_Type(self.driver)
-        self.type.add_new_disease()
+        page_list = self.type.check_disease_page()
+        check.equal(new_additional,page_list[0],"检查新增职务按钮")
+        check.equal(position,page_list[1],"检查职务名称")
+        check.equal(create_time,page_list[2],"检查创建时间")
+        check.equal(modification_time,page_list[3],"检查修改时间")
+        check.equal(operation,page_list[4],"检查操作")
+        log.info("TestCase:验证疾病类型页格式正确<br/>")
+
+    data = [("测试新增疾病类型")]
+    @pytest.mark.parametrize('disease',data)
+    def test_02_add_position(self,disease):
+        '''
+        测试新增疾病类型
+        :param department: 新增疾病类型名
+        '''
+        now_time = time.strftime("%Y_%m_%d_%H_%M_%S")
+        disease = disease + now_time
+        self.type = Disease_Type(self.driver)
+        self.type.add_new_disease(disease)
         log.info("新增疾病类型成功")
-        self.type = Disease_Type(self.driver)
-        self.type.add_none_disease()
-        new_disease_name = self.type.add_none_disease()
-        self.assertEqual(disease_name, new_disease_name+"输入空值失败")
+        time.sleep(1)
+        check.is_true(self.type.find_disease_name(disease),"判断疾病类型是否成功新增，返回true为成功")
+        time.sleep(1)
+        self.type.delete_disease(disease)
+        time.sleep(1)
 
-    @classmethod
-    def tearDownClass(cls) -> None:
-        cls.Type = Disease_Type(cls.driver)
-        cls.Type.delete_disease()
-        cls.driver.quit()
+    data = [("测试修改新增疾病类型","测试修改疾病类型")]
+    @pytest.mark.parametrize('disease,modify_disease',data)
+    def test_03_modify_position(self,disease,modify_disease):
+        '''
+        测试修改疾病类型
+        :param position: 新增疾病类型名称
+        :param modify_position: 修改疾病类型名称
+        '''
+        now_time = time.strftime("%Y_%m_%d_%H_%M_%S")
+        disease = disease + now_time
+        modify_disease = modify_disease + now_time
+        self.type = Disease_Type(self.driver)
+        print(disease,'---------')
+        self.type.add_new_disease(disease)
+        log.info("新增疾病类型成功")
+        self.driver.implicitly_wait(10)
+        res=self.type.find_disease_name(disease)
+        print('%%%',res)
+        time.sleep(1)
+        self.type.modify_disease(disease,modify_disease)
+        check.is_true(self.type.find_disease_name(modify_disease),"判断疾病类型是否成功修改，返回true为成功")
+        time.sleep(1)
+        self.type.delete_disease(modify_disease)
+        time.sleep(1)
+
+    data = [("测试删除新增疾病类型")]
+    @pytest.mark.parametrize('disease',data)
+    def test_04_delete_disease(self,disease):
+        '''
+        测试新增疾病类型
+        :param department: 新增疾病类型名
+        '''
+        now_time = time.strftime("%Y_%m_%d_%H_%M_%S")
+        disease = disease + now_time
+        self.type = Disease_Type(self.driver)
+        self.type.add_new_disease(disease)
+        log.info("新增疾病类型成功")
+        time.sleep(1)
+        print('----------',self.type.find_disease_name(disease))
+        time.sleep(1)
+        self.type.delete_disease(disease)
+        time.sleep(1)
+        check.is_false(self.type.find_disease_name(disease),"判断疾病类型是否成功修改，返回false为成功")
+
+
+        # now_time = time.strftime("%Y_%m_%d_%H_%M_%S")
+        # delete_disease = delete_disease + now_time
+        # self.type = Disease_Type(self.driver)
+        # self.type.add_new_disease(delete_disease)
+        # log.info("新增疾病类型成功")
+        # time.sleep(1)
+        # print(delete_disease)
+        # print(self.type.find_disease_name(delete_disease))
+        # time.sleep(1)
+        # self.type.delete_disease(delete_disease)
+        # time.sleep(1)
+        # check.is_false(self.type.find_disease_name(delete_disease),"判断疾病类型是否成功修改，返回false为成功")
