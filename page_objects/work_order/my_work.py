b5c37052598f1c8ef80677b9a9847f88964be928
秀鹅 <xiu.li@fftai.com>
2022-07-28 18:21:09 +0800

    注释

--- page_objects/work_order/my_work.py
+++ page_objects/work_order/my_work.py
@@ -10,6 +10,10 @@ from utils.droplist_select_tool import Select
 from utils.droplist_select_tool import select_droplist
 from utils.close_tips_tool import left_corner_cancel
 from utils.verify_arrange import verify_arrange
+from selenium.common.exceptions import NoSuchElementException
+from utils.close_tips_tool import close_login_tips
+from utils.close_tips_tool import cancel_button
+from utils.scheduling_conflict_tips_tool import scheduling_conflict
 
 
 log = MyLogging(__name__).logger
@@ -25,9 +29,9 @@ class My_Work():
         self.my_work = NavigateBar(self.driver)
         self.select = Select()
 
-    def start_work(self,item,arrange,time_slot,device):
+    def arrange_work(self,item,arrange,time_slot,device):
         '''
-        工单排班
+        对状态为’未安排‘的工单排班
         :param item: 项目名称
         :param arrange: 排班状态（只点击未排班）
         :param time_slot: 排班的时间段
@@ -43,10 +47,24 @@ class My_Work():
         select_droplist(self.driver,device)
         save = self.version.getLocator(self.driver, 'Save')
         save.click()
+        time.sleep(1)
+        try:
+            tips = self.version.getLocator(self.driver, 'Tips').get_attribute('textContent')
+        except NoSuchElementException:
+            left_corner_cancel(self.driver)
+            assert False, "无提示语，失败！"
+        else:
+            if tips != '排班成功':
+                left_corner_cancel(self.driver)
+                close_login_tips(self.driver)
+                assert False, "新增失败,提示语：%s" % tips
+            else:
+                log.info("新增成功")
+                return tips
 
     def arrange_tips(self,item,arrange,time_slot,device,tips):
         '''
-        工单排班
+        工单排班提示
         :param item: 项目名称
         :param arrange: 排班状态（只点击未排班）
         :param time_slot: 排班的时间段
@@ -60,21 +78,106 @@ class My_Work():
         equipment = self.version.getLocator(self.driver, 'Treatment_Equipment')
         equipment.click()
         select_droplist(self.driver,device)
-        fr_tips = self.driver.find_elements(By.CSS_SELECTOR, value='.fr-tips')
-        time.sleep(1)
-        tips_list = []
-        for fr_tip in fr_tips:
-            if fr_tip.is_displayed():
-                tips_list.append(fr_tip.get_attribute('textContent').strip())
-        time.sleep(1)
-        left_corner_cancel(self.driver)
-        for tip in tips_list:
-            if tip in tips:
-                assert True
-            else:
-                assert False,'当前页面显示%d个;传入的提示语：%s;页面显示提示语：%s'%(len(tips_list),tips,tip)
-        time.sleep(1)
+        scheduling_conflict(self.driver,tips)
+        # fr_tips = self.driver.find_elements(By.CSS_SELECTOR, value='.fr-tips')
+        # time.sleep(1)
+        # tips_list = []
+        # for fr_tip in fr_tips:
+        #     if fr_tip.is_displayed():
+        #         tips_list.append(fr_tip.get_attribute('textContent').strip())
+        # time.sleep(1)
+        # left_corner_cancel(self.driver)
+        # for tip in tips_list:
+        #     if tip in tips:
+        #         assert True
+        #     else:
+        #         assert False,'当前页面显示%d个;传入的提示语：%s;页面显示提示语：%s'%(len(tips_list),tips,tip)
+        # time.sleep(1)
 
+    def grant_arrange_tips(self, item, time_slot, device, tips, operate):
+        '''
+        授权工单排班提示
+        :param item: 项目名称
+        :param arrange: 排班状态（只点击未排班）
+        :param time_slot: 排班的时间段
+        :param device: 治疗设备
+        '''
+        self.my_work.go_to_work_order()
+        time.sleep(1)
+        self.select.checkbox(self.driver, item)
+        self.select.my_order_operation(self.driver, operate)
+        self.select.choose_scheduling(self.driver, time_slot)
+        equipment = self.version.getLocator(self.driver, 'Treatment_Equipment')
+        equipment.click()
+        select_droplist(self.driver, device)
+        scheduling_conflict(self.driver, tips)
+
+    def grant(self,name,arrange,therapist,operate):
+        '''
+        工单授权
+        :param name:治疗项目名称
+        :param arrange:排班状态(未排班)
+        :param therapist:被授权的治疗师名字
+        :param operate:选择的操作（开始/结束/授权/排班）
+        '''
+        self.my_work.go_to_work_order()
+        time.sleep(1)
+        self.select.choose_checkbox_arrange(self.driver,name,arrange)
+        self.select.my_order_operation(self.driver,operate)
+        grant = self.version.getLocator(self.driver, 'Grant')
+        self.driver.execute_script("arguments[0].click();", grant)
+        select_droplist(self.driver,therapist)
+        time.sleep(1)
+        ensure = self.version.getLocator(self.driver, 'Ensure')
+        ensure.click()
+        time.sleep(1)
+        try:
+            tips = self.version.getLocator(self.driver, 'Tips').get_attribute('textContent').strip()
+        except NoSuchElementException:
+            cancel_button(self.driver)
+            assert False, "无提示语，失败！"
+        else:
+            if tips != '授权成功':
+                cancel_button(self.driver)
+                close_login_tips(self.driver)
+                assert False, "新增失败，提示语：%s"%tips
+            else:
+                log.info("新增成功")
+                return tips
+
+    def gran_arrange_work(self,item,operate,time_slot,device):
+        '''
+        对状态为’未安排‘的工单排班
+        :param item: 项目名称
+        :param arrange: 排班状态（只点击未排班）
+        :param time_slot: 排班的时间段
+        :param device: 治疗设备
+        '''
+        self.my_work.go_to_work_order()
+        time.sleep(1)
+        self.select.checkbox(self.driver, item)
+        self.select.my_order_operation(self.driver, operate)
+        time.sleep(1)
+        self.select.choose_scheduling(self.driver,time_slot)
+        equipment = self.version.getLocator(self.driver, 'Treatment_Equipment')
+        equipment.click()
+        select_droplist(self.driver,device)
+        save = self.version.getLocator(self.driver, 'Save')
+        save.click()
+        time.sleep(1)
+        try:
+            tips = self.version.getLocator(self.driver, 'Tips').get_attribute('textContent')
+        except NoSuchElementException:
+            left_corner_cancel(self.driver)
+            assert False, "无提示语，失败！"
+        else:
+            if tips != '排班成功':
+                left_corner_cancel(self.driver)
+                close_login_tips(self.driver)
+                assert False, "新增失败,提示语：%s" % tips
+            else:
+                log.info("新增成功")
+                return tips
 
     def verify_arrang(self,item,arrange_time):
         '''
