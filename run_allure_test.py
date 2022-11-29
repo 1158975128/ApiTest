f842256c327c4ab0aeff6c6ba258f77ef971e334
1158975128 <74446369+1158975128@users.noreply.github.com>
2022-11-29 23:12:46 +0800

    Update run_allure_test.py

--- run_allure_test.py
+++ run_allure_test.py
@@ -5,3 +5,6 @@ import os
 if __name__ == '__main__':
     pytest.main(["--alluredir=./allure-results"])
     os.system("allure generate ./allure-results -o ./allure-report/ --clean-alluredir --clean")
+    
+    
+    
